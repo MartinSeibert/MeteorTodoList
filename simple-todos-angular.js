c4a937352c0a585a07045055f54b440b14619ba2
mas6553 <mas6553@psu.edu>
2015-07-06 22:11:37 -0400

    its done, doesn't work quite right but nbd

--- simple-todos-angular.js
+++ simple-todos-angular.js
@@ -18,18 +18,19 @@ if (Meteor.isClient){
 
   angular.module("simple-todos").controller("TodosListCtrl", ['$scope', '$meteor',
     function($scope, $meteor){
-
+      $scope.$meteorSubscribe("tasks");
       $scope.tasks = $meteor.collection(function(){
         return Tasks.find($scope.getReactively('query'), { sort: { createdAt: -1 } })
 
       });
       $scope.addTask = function(newTask){
-        $scope.tasks.push( {
-          text: newTask,
-          createdAt: new Date(),
-          owner: Meteor.userId(),
-          username: Meteor.user().username}
-        );
+        $meteor.call("addTask", newTask);
+      };
+      $scope.deleteTask = function(task) {
+        $meteor.call("deleteTask", task._id);
+      };
+      $scope.setChecked = function(task){
+        $meteor.call("setChecked", task._id, !task.checked);
       };
       $scope.$watch('hideCompleted', function(){
         if ($scope.hideCompleted){
@@ -43,9 +44,71 @@ if (Meteor.isClient){
       $scope.incompleteCount = function(){
         return Tasks.find({ checked: {$ne: true} }).count();
       };
+      $scope.setPrivate = function(task){
+        $meteor.call("setPrivate", task._id, ! task.private);
+      };
+
     }]);
+
 
     Accounts.ui.config({
       passwordSignupFields: "USERNAME_ONLY"
     });
+}
+
+// code that doesn't run on the client is placed here
+Meteor.methods({
+  addTask: function (text) {
+    // Make sure the user is logged in before inserting a task
+    if (! Meteor.userId()) {
+      throw new Meteor.Error("not-authorized");
+    }
+
+    Tasks.insert({
+      text: text,
+      createdAt: new Date(),
+      owner: Meteor.userId(),
+      username: Meteor.user().username
+    });
+  },
+  deleteTask: function (taskId) {
+    var task = Tasks.findOne(taskId);
+if (task.private && task.owner !== Meteor.userId()) {
+  // If the task is private, make sure only the owner can delete it
+  throw new Meteor.Error("not-authorized");
+}
+    Tasks.remove(taskId);
+  },
+  setChecked: function (taskId, setChecked) {
+    var task = Tasks.findOne(taskId);
+if (task.private && task.owner !== Meteor.userId()) {
+  // If the task is private, make sure only the owner can check it off
+  throw new Meteor.Error("not-authorized");
+}
+
+    Tasks.update(taskId, { $set: { checked: setChecked} });
+  },
+  // Add a method to Meteor.methods called setPrivate
+  setPrivate: function (taskId, setToPrivate) {
+    var task = Tasks.findOne(taskId);
+
+    // Make sure only the task owner can make a task private
+    if (task.owner !== Meteor.userId()) {
+      throw new Meteor.Error("not-authorized");
+    }
+
+    Tasks.update(taskId, { $set: { private: setToPrivate } });
+  }
+});
+
+// code that runs on the server is put here
+if (Meteor.isServer) {
+  Meteor.publish("tasks", function(){
+    return Tasks.find({
+      $or: [
+        { private: {$ne: true}},
+        { owner: this.userId }
+      ]
+    });
+  });
 }
